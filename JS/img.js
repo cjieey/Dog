09d6e878b650d339c0c70807d38bafb205e85757
C3jieey <105692921+cjieey@users.noreply.github.com>
2024-03-21 21:19:01 +0800

    Update img.js

--- JS/img.js
+++ JS/img.js
@@ -1,7 +1,7 @@
 const image = (dogimg) => {
     let div = document.createElement('div')
     div.className = "dogimg"
-    div.innerHTML = `<img src="/images/${dogimg}" alt="dogimg">`
+    div.innerHTML = `<img src="images/${dogimg}" alt="dogimg">`
 
     return div
 }
