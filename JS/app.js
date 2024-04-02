d06eec19167948af9711aec4d268bb2123570bb9
C3jieey <105692921+cjieey@users.noreply.github.com>
2024-04-02 14:21:36 +0800

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -2,7 +2,7 @@ import { homep } from "./info.js";
 import { image } from "./img.js";
 const textcontent = {
     Title: "MY DOG",
-    para: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, reprehenderit amet quidem autem perspiciatis distinctio ducimus quae aliquam minima, harum, illo molestias architecto cupiditate rem.",
+    para: " A golden retriever, often called a "golden," is a friendly and intelligent dog breed known for its beautiful golden coat and gentle temperament. In a shot explanation, we could showcase its friendly demeanor, intelligence, and loyalty through various scenes",
     button: "CONTACT US",
     dogimg: "dog.jpg"
 }
